505562910c3cf178f4a9f8107071d9875e2881d2
maaoor6 <maaoor6@gmail.com>
2020-01-05 17:46:29 +0200

    first commit

---
+++ users.js
@@ -0,0 +1,27 @@
+fetch(
+  "https://gorest.co.in/public-api/users?access-token=MM-P2ZCB1DZEsPHJ6tEleQpSQqnkGLKEKYFL"
+)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.result);
+    let users = data.result;
+    let output = '<div class="row">';
+    users.map(user => {
+      output += `
+                <div class="column">
+                <div class="card">
+              <img src=${user._links.avatar.href} class="center">
+                  <div class="container">
+                    <h2 class="center">${user.first_name} ${user.last_name}</h2>
+                 <i class='fas fa-home' style='font-size:15px'>:${user.address}</i><hr>
+                 <i class="fa fa-phone" style="font-size:15px">:${user.phone}</i><hr>
+                 <i class="fa fa-envelope" style="font-size:15px">:${user.email}</i><hr>
+              </div>
+              </div>
+              </div>
+                `;
+    });
+    output += "</div>";
+    document.getElementById("response").innerHTML = output;
+  })
+  .catch(error => console.error(error));
